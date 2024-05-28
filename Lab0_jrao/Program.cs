54d5f7e1f5e574c80d7c70ece348ed2475a72d8d
= <jazib.rao@edu.sait.ca>
2024-05-27 21:22:11 -0600

    "Updated solution for Tasks 1-3"

--- Lab0_jrao/Program.cs
+++ Lab0_jrao/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,25 +12,81 @@ namespace Lab0_jrao
     {
         static void Main()
         {
-            Console.WriteLine("Enter a low number");
-            int low_num = Convert.ToInt32(Console.ReadLine());
+            int high_num = 0;
+            int low_num = 0;
+            List<string> lines = new List<string>();
+
             while (low_num <= 0)
             {
                 Console.WriteLine("Enter a low number");
-                low_num = Convert.ToInt32(Console.ReadLine());
-            }
-            Console.WriteLine("Enter a high number");
-            int high_num = Convert.ToInt32(Console.ReadLine());
+                String low_input = Console.ReadLine();
 
+                //check if input is an int
+                if (CheckInt(low_input))
+                {
+                    low_num = Convert.ToInt32(low_input);
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number");
+                }
+            }
+            
             while (high_num <= low_num)
             {
                 Console.WriteLine("Enter a high number");
-                high_num = Convert.ToInt32(Console.ReadLine());
+                String high_input = Console.ReadLine();
+
+                //check if input is an int
+                if (CheckInt(high_input))
+                {
+                    high_num = Convert.ToInt32(high_input);
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number");
+                }
             }
 
+            //calculate the difference between low and high numbers
             int diff = high_num - low_num;
+
             Console.WriteLine("The difference is " + diff);
+
+            //calculate all numbers between low and high num
+            int[] num_array = Enumerable.Range(low_num+1, diff-1).ToArray();
+
+            //reverse the array
+            Array.Reverse(num_array);
+
+            //filepath of text file
+            String filepath = @"C:\Users\Jazz\source\repos\Lab0_jrao\nums.txt";
+
+            //for each number in array, add to lines list
+            foreach (int num in num_array)
+            {
+                lines.Add(num.ToString());
+            }
+
+            //write all ints to txt file
+            File.WriteAllLines(filepath, lines);
+            
+            //read all numbers from txt file
+            String[] numbers = File.ReadAllLines(filepath);
+            
+            foreach(string line in numbers)
+            {
+                Console.WriteLine(line);
+            }
+            
             Console.ReadLine();
+            
+        }
+        //method to check validity of input to see if its an int
+        public static bool CheckInt(string input)
+        {
+            int temp;
+            return int.TryParse(input, out temp);
         }
     }
 }
